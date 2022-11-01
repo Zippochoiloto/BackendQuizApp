d592e62ddc0cdaddb43e06bbb9f31b825a279048
NgocBaoTa <tabaongocthachthat8a@gmail.com>
2022-11-01 18:20:09 +0700

    check api

--- routes/category.js
+++ routes/category.js
@@ -6,31 +6,41 @@ categoryRouter.get("/", async (req, res) => {
   try {
     const level = req.headers.level;
     const name = req.headers.name;
-    const interested = req.headers.interested;
+    // const interested = req.headers.interested;
     let category;
-    if (name) {
-      category = await db.Categories.find({
-        name,
-      })
-        .sort({ interested: -1 })
-        .toArray();
-    } else if (level) {
-      category = await db.Categories.find({
-        level,
-      })
-        .sort({ interested: -1 })
-        .toArray();
-    } else if (interested) {
-      // let interestedString = interested.toString()
-      let interestedBool = interested === "true";
-      category = await db.Categories.find({
-        interested: interestedBool,
-      }).toArray();
-    } else {
-      category = await db.Categories.find({})
-        .sort({ interested: -1 })
-        .toArray();
+    // if (name) {
+    //   category = await db.Categories.find({
+    //     name,
+    //   })
+    //     .sort({ interested: -1 })
+    //     .toArray();
+    // } else if (level) {
+    //   category = await db.Categories.find({
+    //     level,
+    //   })
+    //     .sort({ interested: -1 })
+    //     .toArray();
+    // } else if (interested) {
+    //   // let interestedString = interested.toString()
+    //   let interestedBool = interested === "true";
+    //   category = await db.Categories.find({
+    //     interested: interestedBool,
+    //   }).toArray();
+    // } else {
+    //   category = await db.Categories.find({})
+    //     .sort({ interested: -1 })
+    //     .toArray();
+    // }
+    let query = {};
+    // const sort = {}
+    if (level) {
+      query["level"] = level;
     }
+    if (name) {
+      query["name"] = name;
+    }
+    // if (interested) sort(interested) = 1
+    category = await db.todos.find(query).sort({ interested: -1 }).toArray();
     res.status(201);
     res.json(category);
   } catch (error) {
@@ -39,16 +49,14 @@ categoryRouter.get("/", async (req, res) => {
   }
 });
 
-
 categoryRouter.get("/quiz", async (req, res) => {
   try {
     const name = req.headers.name;
-    let array;
-    let quiz; 
+    let quiz;
     if (name) {
-      array = await db.Categories.aggregate([
+      quiz = await db.Categories.aggregate([
         {
-          $match: {name},
+          $match: { name },
         },
         {
           $lookup: {
@@ -60,8 +68,8 @@ categoryRouter.get("/quiz", async (req, res) => {
         },
         {
           $unwind: {
-            path: "$questions"
-          }
+            path: "$questions",
+          },
         },
         {
           $project: {
@@ -70,10 +78,6 @@ categoryRouter.get("/quiz", async (req, res) => {
           },
         },
       ]).toArray();
-
-      quiz = array.map((item) => {
-        return item.questions;
-      });
     } else {
       quiz = await db.Quiz.find({}).sort({ order: 1 }).toArray();
     }
