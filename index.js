2907abd89d1a9154ef31671f56e887eec0addda7
Diec Trac Lam <diectraclam.0812@gmail.com>
2022-11-04 19:31:08 +0700

    Register Admin

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const router = require("./routes");
@@ -14,7 +14,7 @@ app.use(
 );
 app.use(bodyParser.json());
 // app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
 app.use("/categories", categoryRouter);
 
