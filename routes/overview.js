1074521487acef4884fd72351f2ec74b20c1a8ee
NgocBaoTa <tabaongocthachthat8a@gmail.com>
2022-11-29 19:46:45 +0700

    api get dashboard overview

--- routes/overview.js
+++ routes/overview.js
@@ -5,18 +5,64 @@ const OverviewRouter = express.Router();
 OverviewRouter.get("/", async (req, res) => {
     try {
         let data = {};
-        let totalQuizes;
-        totalQuizes = await db.Quiz.find({}).toArray();
-      //   data.totalUsers = await db.User.find({}).toArray().length;
-      //   data.totalAdmins = await db.Admin.find({}).toArray().length;
-      // data.totalCategories = await db.Categories.find({}).toArray().length;
+        data.totalQuizes = await db.Quiz.find({}).count()
+        data.totalUsers = await db.User.find({}).count();
+        data.totalAdmins = await db.Admin.find({}).count();
+        data.totalCategories = await db.Categories.find({}).count();
       
     res.status(200);
-    res.json(totalQuizes);
+    res.json(data);
   } catch (error) {
     res.status(500);
     res.send(error.message);
   }
 })
+
+OverviewRouter.get("/top-category", async (req, res) => {
+  try {
+    let data = {};
+    let array = await db.Categories.aggregate([
+      {
+        $lookup: {
+          from: "Quiz",
+          localField: "_id",
+          foreignField: "categoryId",
+          as: "questions",
+        },
+      },
+      {
+        $group: {
+          _id: "$name",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          cate: "$_id",
+          count: "$count",
+        },
+      },
+      {
+        $sort: {
+          count: -1,
+        },
+      },
+      {
+        $skip: 0
+      }, 
+      {
+        $limit: 4
+      }
+    ]).toArray();
+    
+
+    res.status(200);
+    res.json(array);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+});
 
 module.exports = OverviewRouter;
